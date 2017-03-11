b39a583edfc2b8739331ea4659b5704cc1ee2f4d
LeRoy, David <david_leroy@intuit.com>
2017-03-11 06:33:57 -0800

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by LeRoy, David on 3/11/17.
+//  Copyright © 2017 LeRoy, David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
